2af4441873a23911d1944362e01a6f5d0ea4da2e
unknown <gagas112212@gmail.com>
2023-11-23 14:41:19 +0700

    jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,20 @@
+import { JwtService } from '@nestjs/jwt';
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
+  imports: [
+    PrismaModule,
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+      property: 'user',
+      session: false,
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,83 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { RegisterDto } from './dto/register.dto';
+import { compare, hash } from 'bcrypt';
+import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
+import { jwt_config } from 'src/config/config_jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
+
+  // Register newUser
+  // @param data
+  // @returns
+
+  generateJWT(payload: any) {
+    return this.jwtService.sign(payload, {
+      secret: jwt_config.secret,
+      expiresIn: jwt_config.expired,
+    });
+  }
+  async register(data: RegisterDto) {
+    const checkUserExists = await this.prisma.users.findFirst({
+      where: {
+        email: data.email,
+      },
+    });
+    if (checkUserExists) {
+      throw new HttpException(
+        `User ${data.email} already exists`,
+        HttpStatus.FOUND,
+      );
+    }
+    data.password = await hash(data.password, 12);
+
+    const createUser = await this.prisma.users.create({
+      data: data,
+    });
+    if (createUser) {
+      return {
+        statusCode: HttpStatus.CREATED,
+        message: 'Register Successfully',
+      };
+    }
+  }
+
+  async login(data: LoginDto) {
+    const checkUser = await this.prisma.users.findFirst({
+      where: {
+        email: data.email,
+      },
+    });
+    if (!checkUser) {
+      throw new HttpException(
+        `User ${data.email} not found`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    const checkPassword = await compare(data.password, checkUser.password);
+
+    if (checkPassword) {
+      const accessToken = this.generateJWT({
+        sub: checkUser.id,
+        name: checkUser.name,
+        email: checkUser.email,
+      });
+      return {
+        statusCode: 200,
+        accessToken,
+        message: 'Login Berhasil',
+      };
+    } else {
+      throw new HttpException(
+        'User or Password incorrect',
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+  }
+}
